1857e43ebcff6efef014e2771096dec003597ca0
bhavesh <bhaveshadtani2001@gmail.com>
2024-09-20 11:49:26 +0530

    login register routes created

--- BE/routes/user.js
+++ BE/routes/user.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
-const { login } = require("../controllers/user.js");
+const { login, register } = require("../controllers/user.js");
+const verify = require("../middleware/verify.js");
 
-router.route("/login").get(login);
+router.route("/login").post(login);
+router.route("/register").post(register);
+
+// router.route("/forgot-password").post(forgot-password);
+// router.route("/reset-password").post(reset-password);
+// router.route("/logout").post(logout);
 
 module.exports = router;
