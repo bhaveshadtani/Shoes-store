64ff73419f09f4814b3043aeaed9624221b739c2
bhavesh <bhaveshadtani2001@gmail.com>
2024-10-08 16:02:22 +0530

    created middleware for user authentication

--- BE/middleware/verify.js
+++ BE/middleware/verify.js
@@ -9,7 +9,13 @@ const verify = async (req, res, next) => {
     return res.status(403).json({ message: "Token is required" });
   }
   try {
-    
+    const decoded = await promisify(jwt.verify)(token, secretKey);
+    const user = await db.user.findOne({ where: { id: decoded.userId } });
+    if (!user) {
+      throw new Error("User not found");
+    }
+    req.user = user;
+    next();
   } catch (error) {
     console.error("JWT Verification Error:", error);
     return res.status(401).json({ message: "Invalid token" });
