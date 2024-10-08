9fd41613757d743b211543f5db391d19871281e7
bhavesh <bhaveshadtani2001@gmail.com>
2024-10-08 15:58:44 +0530

    added cart routes and storing data of not logged in user's in cookie

--- BE/server.js
+++ BE/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 var bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 require("dotenv").config();
@@ -13,12 +14,14 @@ app.use(
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json()); // To parse JSON request bodies
+app.use(cookieParser()); // To parse cookies
 
 app.use("/users", require("./routes/user"));
 app.use("/products", require("./routes/product"));
+app.use("/cart", require("./routes/cart"));
 // app.use("/orders", require("./routes/order"));
 // app.use("/reviews", require("./routes/review"));
-// app.use("/cart", require("./routes/cart"));
 // app.use("/checkout", require("./routes/checkout"));
 // app.use("/shipping", require("./routes/shipping"));
 // app.use("/returns", require("./routes/return"));
