16d99c85817745efc302514eba10aa5c81790e80
bhavesh <bhaveshadtani2001@gmail.com>
2024-10-17 15:09:33 +0530

    created cart routes

--- BE/routes/cart.js
+++ BE/routes/cart.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const { addCart, viewCart, removeCart } = require("../controllers/cart");
+const verify = require("../middleware/verify");
 
-router.route("/add-cart").post(addCart);
-router.route("/view-cart").post(viewCart);
-router.route("/remove-cart").post(removeCart);
+router.route("/add-cart").post(verify, addCart);
+router.route("/view-cart").post(verify, viewCart);
+router.route("/remove-cart").post(verify, removeCart);
 module.exports = router;
