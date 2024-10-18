8bfcd6459216a631c51ade8936a5482e22207aa5
bhavesh <bhaveshadtani2001@gmail.com>
2024-10-18 15:52:48 +0530

    added price field in productVariation table and some changes in product helper file

--- BE/models/productVariation.js
+++ BE/models/productVariation.js
@@ -1,39 +1,52 @@
 module.exports = (sequelize, DataTypes) => {
-  const ProductVariation = sequelize.define("productVariation", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    product_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "Product",
-        key: "id",
+  const ProductVariation = sequelize.define(
+    "productVariation",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      product_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "Product",
+          key: "id",
+        },
+      },
+      size_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "Size",
+          key: "id",
+        },
+      },
+      color_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "Color",
+          key: "id",
+        },
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      unit_price: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+        defaultValue: 0.0,
+        validate: {
+          isDecimal: true, // Ensure price is a decimal
+          min: 0, // Ensure price is non-negative
+        },
       },
     },
-    size_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "Size",
-        key: "id",
-      },
-    },
-    color_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "Color",
-        key: "id",
-      },
-    },
-    quantity: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-    },
-  },{
-    timestamps: false,
-  });
+    {
+      timestamps: false,
+    }
+  );
 
   // ProductVariation.sync({alter: true})
   return ProductVariation;
