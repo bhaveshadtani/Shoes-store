32d14d16149ed79511c466ff4011fa238af185cb
bhavesh <bhaveshadtani2001@gmail.com>
2024-11-27 12:37:50 +0530

    image data updated

--- BE/helpers/imageData.js
+++ BE/helpers/imageData.js
@@ -1,11 +1,11 @@
 const imageData = [
   {
-    url: "https://example.com/image1-1.jpg",
+    url: "https://unsplash.com/photos/blue-trainers-on-colorful-leaves-on-the-ground-autumn-nature-kYfWXbyuh-s",
     is_main: true,
     product_variant_id: 1,
   },
   {
-    url: "https://example.com/image1-2.jpg",
+    url: "https://images.unsplash.com/photo-1537636568536-a2e00b44cb85?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     is_main: false,
     product_variant_id: 1,
   },
@@ -26,7 +26,7 @@ const imageData = [
   },
 
   {
-    url: "https://example.com/image2-1.jpg",
+    url: "https://images.unsplash.com/photo-1578986175247-7d60c6df07c5?q=80&w=2094&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     is_main: true,
     product_variant_id: 2,
   },
@@ -52,7 +52,7 @@ const imageData = [
   },
 
   {
-    url: "https://example.com/image3-1.jpg",
+    url: "https://images.unsplash.com/photo-1580906853149-f82f7601d205?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     is_main: true,
     product_variant_id: 3,
   },
@@ -62,7 +62,7 @@ const imageData = [
     product_variant_id: 3,
   },
   {
-    url: "https://example.com/image3-3.jpg",
+    url: "https://images.unsplash.com/photo-1530389912609-9a007b3c38a4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     is_main: false,
     product_variant_id: 3,
   },
@@ -78,7 +78,7 @@ const imageData = [
   },
 
   {
-    url: "https://example.com/image4-1.jpg",
+    url: "https://plus.unsplash.com/premium_photo-1663100769321-9eb8fe5a8e6b?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     is_main: true,
     product_variant_id: 4,
   },
@@ -104,7 +104,7 @@ const imageData = [
   },
 
   {
-    url: "https://example.com/image5-1.jpg",
+    url: "https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     is_main: true,
     product_variant_id: 5,
   },
@@ -130,7 +130,7 @@ const imageData = [
   },
 
   {
-    url: "https://example.com/image6-1.jpg",
+    url: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     is_main: true,
     product_variant_id: 6,
   },
@@ -156,7 +156,7 @@ const imageData = [
   },
 
   {
-    url: "https://example.com/image7-1.jpg",
+    url: "https://images.unsplash.com/photo-1561909848-977d0617f275?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     is_main: true,
     product_variant_id: 7,
   },
@@ -182,7 +182,7 @@ const imageData = [
   },
 
   {
-    url: "https://example.com/image8-1.jpg",
+    url: "https://images.unsplash.com/photo-1485736231968-0c8ad5c9e174?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     is_main: true,
     product_variant_id: 8,
   },
@@ -208,7 +208,7 @@ const imageData = [
   },
 
   {
-    url: "https://example.com/image9-1.jpg",
+    url: "https://images.unsplash.com/photo-1529810313688-44ea1c2d81d3?q=80&w=2141&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     is_main: true,
     product_variant_id: 9,
   },
@@ -234,7 +234,7 @@ const imageData = [
   },
 
   {
-    url: "https://example.com/image10-1.jpg",
+    url: "https://images.unsplash.com/photo-1543508282-6319a3e2621f?q=80&w=2030&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     is_main: true,
     product_variant_id: 10,
   },
@@ -260,7 +260,7 @@ const imageData = [
   },
 
   {
-    url: "https://example.com/image11-1.jpg",
+    url: "https://images.unsplash.com/photo-1465453869711-7e174808ace9?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     is_main: true,
     product_variant_id: 11,
   },
@@ -286,7 +286,7 @@ const imageData = [
   },
 
   {
-    url: "https://example.com/image12-1.jpg",
+    url: "https://images.unsplash.com/photo-1547701865-941c3731e8af?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     is_main: true,
     product_variant_id: 12,
   },
@@ -312,7 +312,7 @@ const imageData = [
   },
 
   {
-    url: "https://example.com/image13-1.jpg",
+    url: "https://images.unsplash.com/photo-1518362165686-c587a1de1003?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     is_main: true,
     product_variant_id: 13,
   },
@@ -338,7 +338,7 @@ const imageData = [
   },
 
   {
-    url: "https://example.com/image14-1.jpg",
+    url: "https://images.unsplash.com/photo-1552066344-2464c1135c32?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     is_main: true,
     product_variant_id: 14,
   },
@@ -364,7 +364,7 @@ const imageData = [
   },
 
   {
-    url: "https://example.com/image15-1.jpg",
+    url: "https://images.unsplash.com/photo-1558004282-e2b2587e3e47?q=80&w=1925&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     is_main: true,
     product_variant_id: 15,
   },
@@ -390,7 +390,7 @@ const imageData = [
   },
 
   {
-    url: "https://example.com/image16-1.jpg",
+    url: "https://images.unsplash.com/photo-1575537302964-96cd47c06b1b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     is_main: true,
     product_variant_id: 16,
   },
@@ -416,7 +416,7 @@ const imageData = [
   },
 
   {
-    url: "https://example.com/image17-1.jpg",
+    url: "https://images.unsplash.com/photo-1460353581641-37baddab0fa2?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     is_main: true,
     product_variant_id: 17,
   },
@@ -442,7 +442,7 @@ const imageData = [
   },
 
   {
-    url: "https://example.com/image18-1.jpg",
+    url: "https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     is_main: true,
     product_variant_id: 18,
   },
@@ -468,7 +468,7 @@ const imageData = [
   },
 
   {
-    url: "https://example.com/image19-1.jpg",
+    url: "https://images.unsplash.com/photo-1514989940723-e8e51635b782?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     is_main: true,
     product_variant_id: 19,
   },
@@ -494,7 +494,7 @@ const imageData = [
   },
 
   {
-    url: "https://example.com/image20-1.jpg",
+    url: "https://images.unsplash.com/photo-1579446565308-427218a2c60e?q=80&w=1925&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     is_main: true,
     product_variant_id: 20,
   },
@@ -520,7 +520,7 @@ const imageData = [
   },
 
   {
-    url: "https://example.com/image21-1.jpg",
+    url: "https://images.unsplash.com/photo-1579338559194-a162d19bf842?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     is_main: true,
     product_variant_id: 21,
   },
